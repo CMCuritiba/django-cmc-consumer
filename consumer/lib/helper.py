0c11fea5a99ff2604e84311d18ec57ea919bb5ff
aleodoni <aleodoni@gmail.com>
2018-10-11 16:50:48 -0300

    correção identação

--- consumer/lib/helper.py
+++ consumer/lib/helper.py
@@ -2,56 +2,56 @@ from .msconsumer import MSCMCConsumer, Funcionario, Setor, Vereador, Vereadores_
 
 class ServiceHelper(object):
 
-	cons = MSCMCConsumer()
+    cons = MSCMCConsumer()
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna setores consumidos
-	# ----------------------------------------------------------------------------------------------------------------
-	def get_setores(self):
-		setores = self.cons.consome_setores()
-		objetos_setores = []
-		for line in setores:
-			objetos_setores.append(Setor(line['set_id'], line['set_nome'], line['set_sigla'], line['set_id_superior'], line['set_ativo'], line['set_tipo']))
-		return objetos_setores
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna setores consumidos
+    # ----------------------------------------------------------------------------------------------------------------
+    def get_setores(self):
+        setores = self.cons.consome_setores()
+        objetos_setores = []
+        for line in setores:
+            objetos_setores.append(Setor(line['set_id'], line['set_nome'], line['set_sigla'], line['set_id_superior'], line['set_ativo'], line['set_tipo']))
+        return objetos_setores
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna setor consumido
-	# ----------------------------------------------------------------------------------------------------------------
-	def get_setor(self,set_id):
-		return self.cons.consome_setor_setor(set_id)
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna setor consumido
+    # ----------------------------------------------------------------------------------------------------------------
+    def get_setor(self,set_id):
+        return self.cons.consome_setor_setor(set_id)
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna funcionarios consumidos
-	# ----------------------------------------------------------------------------------------------------------------
-	def get_funcionarios(self):
-		funcionarios = self.cons.consome_funcionarios()
-		objetos_funcionarios = []
-		for line in funcionarios:
-			objetos_funcionarios.append(Funcionario(line['matricula'], line['pessoa'], line['pes_nome'], line['funcao'], line['set_id'], line['ind_estagiario']))
-		return objetos_funcionarios
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna funcionarios consumidos
+    # ----------------------------------------------------------------------------------------------------------------
+    def get_funcionarios(self):
+        funcionarios = self.cons.consome_funcionarios()
+        objetos_funcionarios = []
+        for line in funcionarios:
+            objetos_funcionarios.append(Funcionario(line['matricula'], line['pessoa'], line['pes_nome'], line['funcao'], line['set_id'], line['ind_estagiario']))
+        return objetos_funcionarios
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna funcionario consumido
-	# ----------------------------------------------------------------------------------------------------------------		
-	def get_funcionario(self, pessoa):
-		return self.cons.consome_funcionario(pessoa)
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna funcionario consumido
+    # ----------------------------------------------------------------------------------------------------------------      
+    def get_funcionario(self, pessoa):
+        return self.cons.consome_funcionario(pessoa)
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna setores consumidos formatados para combo box
-	# ----------------------------------------------------------------------------------------------------------------
-	def get_setores_combo(self, inicial):
-		setores = self.cons.consome_setores()
-		objetos_setores = []
-		objetos_setores.append(Setor(None, inicial, inicial, None, None, None))
-		for line in setores:
-			objetos_setores.append(Setor(line['set_id'], line['set_nome'], line['set_sigla'], line['set_id_superior'], line['set_ativo'], line['set_tipo']))
-		return objetos_setores
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna setores consumidos formatados para combo box
+    # ----------------------------------------------------------------------------------------------------------------
+    def get_setores_combo(self, inicial):
+        setores = self.cons.consome_setores()
+        objetos_setores = []
+        objetos_setores.append(Setor(None, inicial, inicial, None, None, None))
+        for line in setores:
+            objetos_setores.append(Setor(line['set_id'], line['set_nome'], line['set_sigla'], line['set_id_superior'], line['set_ativo'], line['set_tipo']))
+        return objetos_setores
 
-	# ----------------------------------------------------------------------------------------------------------------
-	# Retorna textos de conclusão dos projetos
-	# ----------------------------------------------------------------------------------------------------------------		
-	def get_textos_conclusao(self, pro_codigo):
-		return self.cons.consome_textos_conclusao(pro_codigo)
+    # ----------------------------------------------------------------------------------------------------------------
+    # Retorna textos de conclusão dos projetos
+    # ----------------------------------------------------------------------------------------------------------------      
+    def get_textos_conclusao(self, pro_codigo):
+        return self.cons.consome_textos_conclusao(pro_codigo)
 
     # ----------------------------------------------------------------------------------------------------------------
     # Retorna setores consumidos
